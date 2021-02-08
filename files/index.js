894f9ca8eb013fe327c9c87c1a1c8fc3736b095c
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-07 22:32:44 -0300

    preparing file update

---
+++ files/index.js
@@ -0,0 +1,23 @@
+class i18 {
+
+    // Constructor
+    constructor() { return require('./constructor').apply(this, arguments); }
+
+    // Get Lang File
+    getFile() { return require('./getFile').apply(this, arguments); }
+
+    // Get Lang List
+    getLangList() { return this.list; }
+
+    // Get Lang List
+    getDefaultLang() { return this.defaultLang; }
+
+    // Get Selected Lang
+    getSelectedLang() { return require('./getSelectedLang').apply(this, arguments); }
+
+    // Get User Lang
+    getUserLang() { return require('./getUserLang').apply(this, arguments); }
+
+}
+
+module.exports = i18;
--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-class i18 {
+class expressTimezone {
 
     // Constructor
-    constructor() { return require('./files/constructor').apply(this, arguments); }
+    constructor(app, data) {
 
-    // Get Lang File
-    getFile() { return require('./files/getFile').apply(this, arguments); }
+        // Insert App
+        this.app = app;
 
-    // Get Lang List
-    getLangList() { return this.list; }
+        // Lodash Module
+        const _ = require('lodash');
+        this.cfg = _.defaultsDeep({}, data, {
+            varNames: {
+                sessionLang: 'sessionLang',
+                userLang: 'userLang',
+                nowLang: 'nowLang'
+            }
+        });
 
-    // Get Lang List
-    getDefaultLang() { return this.defaultLang; }
+        // Prepare Module
+        this.module = require('./files');
 
-    // Get Selected Lang
-    getSelectedLang() { return require('./files/getSelectedLang').apply(this, arguments); }
+        // Complete
+        return this;
 
-    // Get User Lang
-    getUserLang() { return require('./files/getUserLang').apply(this, arguments); }
+    }
 
-}
+    // Get Module
+    getModule() { return this.module; }
 
-module.exports = i18;
+    // Insert Express
+    insert() {
+        const tinyThis = this;
+        return (req, res, next) => {
+
+            // Get User Lang
+            const userLang = tinyThis.i18.getUserLang({
+                session: req.session[tinyCfg.varNames.sessionLang],
+                user: req.session[tinyCfg.varNames.userLang]
+            });
+
+            // Set Session
+            req.session[tinyCfg.varNames.sessionLang] = userLang.session;
+            req.session[tinyCfg.varNames.userLang] = userLang.user;
+            req.session[tinyCfg.varNames.nowLang] = userLang.now;
+
+            // Complete
+            next();
+            return;
+
+        };
+    }
+
+};
+
+module.exports = expressTimezone;
