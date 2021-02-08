03fdad0ab1c44d16a3d2aadc712909641f5f26e2
Yasmin Seidel <jackiedevpe@outlook.com.br>
2021-02-08 00:23:39 -0300

    get user lang fixed

--- api/getUserLang.js
+++ api/getUserLang.js
@@ -7,22 +7,39 @@ module.exports = function (data) {
         session: this.defaultLang
     };
 
-    // User Session
-    if (typeof data.user === "string") {
-        tinyResult.now = data.user.replace(/\//g, '').replace(/\\/g, '');
-        tinyResult.user = tinyResult.now;
-    } else if (typeof data.session === "string") {
-        tinyResult.now = data.session.replace(/\//g, '').replace(/\\/g, '');
-        tinyResult.session = tinyResult.now;
-    }
+
 
     // Is User
     if (data.isUser && (data.isUser === "on" || data.isUser === "true" || data.isUser === true)) {
+
+        // Set Is User Value
         tinyResult.isUser = true;
         this.isUser = true;
-    } else {
+
+        // User Session
+        if (typeof data.user === "string") {
+            tinyResult.now = data.user.replace(/\//g, '').replace(/\\/g, '');
+            tinyResult.user = tinyResult.now;
+        } else if (typeof data.session === "string") {
+            tinyResult.now = data.session.replace(/\//g, '').replace(/\\/g, '');
+            tinyResult.user = tinyResult.now;
+        }
+
+    }
+
+    // Nope
+    else {
+
+        // Set Is User Value
         tinyResult.isUser = false;
         this.isUser = false;
+
+        // User Session
+        if (typeof data.session === "string") {
+            tinyResult.now = data.session.replace(/\//g, '').replace(/\\/g, '');
+            tinyResult.session = tinyResult.now;
+        }
+
     }
 
     // User Lang
